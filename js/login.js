89415d650f5c9ea19961698a50824f34df83386e
Linderlly  Santana <148879489+Linderlly@users.noreply.github.com>
2024-11-07 15:31:12 -0400

    Mundança no link do login js

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ const handleSubmit = (event) => {
   event.preventDefault();
 
   localStorage.setItem('player', input.value);
-  window.location = './html/game.html';
+  window.location.href = './html/game.html';
 }
 
 input.addEventListener('input', validateInput);
