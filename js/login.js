2323575d7667e85730116621a00c242ba4deab0e
Linderlly  Santana <148879489+Linderlly@users.noreply.github.com>
2024-11-07 14:29:18 -0400

    Alterações_Links

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ const handleSubmit = (event) => {
   event.preventDefault();
 
   localStorage.setItem('player', input.value);
-  window.location = '../html/game.html';
+  window.location = './html/index.html';
 }
 
 input.addEventListener('input', validateInput);
